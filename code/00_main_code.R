ca9f31d74089bf05dae5dad28368a0c9cd52f7c5
gabrielcaser <gabrielcaser9@hotmail.com>
2025-11-09 19:41:38 -0300

    covid data working

--- code/00_main_code.R
+++ code/00_main_code.R
@@ -57,7 +57,7 @@ data_2016 = paste0("rdd_data_", non_stem_college, "_", "2016_", stem_definition,
 source("code/creates_covid_data.R")
 source("code/01_create_dataset.R")
 #source("code/02_sum_stats.R")
-source("code/03_regressions_main.R")
+#source("code/03_regressions_main.R")
 #source("code/04_regressions_moderation.R")
 #source("code/05_regressions_moderation_loops.R")
 #source("code/06_parameters_latexR.R")
